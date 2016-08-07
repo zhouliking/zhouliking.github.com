2fd7edd6f8edcedb3cc1db23692edb7a16e3a7e4
zhouli <545289437@qq.com>
2016-08-07 23:22:35 +0800

    zl

--- statics/js/index.js
+++ statics/js/index.js
@@ -56,6 +56,17 @@ $(document).ready(function(){
         }
     });
 })();
+
+var duoshuoQuery = {short_name:"zlresource"};
+(function() {
+				var ds = document.createElement('script');
+				ds.type = 'text/javascript';ds.async = true;
+				ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+				ds.charset = 'UTF-8';
+				(document.getElementsByTagName('head')[0] 
+				 || document.getElementsByTagName('body')[0]).appendChild(ds);
+})();
+
 });
 
 
